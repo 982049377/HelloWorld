73e54791eeb5c05a5aebf5271a1beba3937804c0
王恒尊 <982049377@qq.com>
2016-09-19 22:17:42 +0800

    一维数组暂时存储进行解决

---
+++ hello.js
@@ -0,0 +1,29 @@
+// TypeScript file
+var nn;
+var Arraya;
+var k;
+var l;
+var m;
+Arraya = [];
+nn = 3;
+m = 2 * nn - 1;
+initialize();
+set();
+function initialize() {
+    for (k = 0; k < m; k++)
+        Arraya.push(" ");
+    for (l = 0; l < nn; l++) {
+        console.log(Arraya);
+        console.log();
+    }
+}
+function set() {
+    for (k = 0; k < nn; k++) {
+        Arraya[(m - 1) / 2] = "*";
+        for (l = 0; l <= k; l++) {
+            Arraya[(m - 1) / 2 - l] = "*";
+            Arraya[(m - 1) / 2 + l] = "*";
+        }
+        console.log(Arraya);
+    }
+}
---
+++ hello.ts
@@ -0,0 +1,29 @@
+// TypeScript file
+var nn:number;
+var  Arraya:string[];
+var k:number;
+var l:number;
+var m:number;
+Arraya=[];
+nn=3;
+m=2*nn-1;
+initialize()
+set();
+function initialize():void
+{
+    for(k=0;k<m;k++)
+    Arraya.push(" ");
+}
+function set():void
+{
+    for(k=0;k<nn;k++)
+    {  
+        Arraya[(m-1)/2]="*";
+        for(l=0;l<=k;l++)
+        {
+            Arraya[(m-1)/2-l]="*";
+            Arraya[(m-1)/2+l]="*";
+        }
+        console.log(Arraya);
+    }          
+}
